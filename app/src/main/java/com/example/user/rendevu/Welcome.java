b85d539f7a11d0781aeabbc526ad7f428f3493fe
rendevudev <rendevu.firebase@gmail.com>
2018-02-03 19:03:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/user/rendevu/Welcome.java
@@ -0,0 +1,15 @@
+package com.example.user.rendevu;
+/*
+    Josh Davenport
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
---
+++ app/src/main/java/com/example/user/rendevu/cardAdapter.java
@@ -0,0 +1,64 @@
+package com.example.user.rendevu;
+/*
+    Ricardo Cantu
+    This class holds adapter configurations for the recycle view
+ */
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class cardAdapter extends RecyclerView.Adapter<cardAdapter.PersonViewHolder> {
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PersonViewHolder(View itemView){
+            super(itemView);
+            cardView = (CardView)itemView.findViewById(R.id.card_view);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+
+        }
+    }
+
+    List<Person> persons;
+    cardAdapter(List<Person> persons){
+        this.persons = persons;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView){
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_item, viewGroup, false);
+        PersonViewHolder personViewHolder = new PersonViewHolder(view);
+        return personViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i){
+        personViewHolder.personName.setText(persons.get(i).name);
+        personViewHolder.personAge.setText(persons.get(i).age);
+        personViewHolder.personPhoto.setImageResource(persons.get(i).photoID);
+    }
+
+    @Override
+    public int getItemCount(){
+        return persons.size();
+    }
+}
+
