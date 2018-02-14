4b75b90d2507f265501d0a9d82ca1101b0da8b5c
Chris gutierrez <chris201234@gmail.com>
2018-02-14 02:01:29 -0600

    On branch rendevu-dev_recentLocation 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/rendevu/main/Main2Activity.java 	new file:   app/src/main/res/values/.strings.xml.swp 	modified:   app/src/main/res/values/strings.xml
    Included the LastLocation setting
    will put in comments to better explain this later

--- app/src/main/java/com/rendevu/main/Main2Activity.java
+++ app/src/main/java/com/rendevu/main/Main2Activity.java
@@ -16,8 +16,10 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Looper;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -67,6 +69,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
 import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -79,13 +84,15 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 //import javax.xml.crypto.Data;
 
 
-public class Main2Activity extends AppCompatActivity implements MyDialogFragment.UserNameListener {
+public class Main2Activity extends AppCompatActivity implements MyDialogFragment.UserNameListener, ActivityCompat.OnRequestPermissionsResultCallback {
+    private static final int PERMISSION_REQUEST_LOCATION = 34;
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -106,9 +113,11 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
 
     private static GoogleApiClient mGoogleApiClient;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
+
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main2);
             // Create the adapter that will return a fragment for each of the three
@@ -132,16 +141,17 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
                     .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
                     .addApi(LocationServices.API)
                     .build();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
     @Override
     protected void onStart() {
         super.onStart();
+
     }
+
+
 
     @Override
     protected void onStop() {
@@ -347,13 +357,17 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
     public static class MainScreenTabFragment extends Fragment implements OnMapReadyCallback {
 
         FirebaseDatabase database;
-        private List<Tracking> list;
-
         Marker marker;
         MapView mapView;
-
+        private List<Tracking> list;
         ToggleButton saveLocButton;
 
+        private FusedLocationProviderClient mFusedLocationClient;
+        final double defaultLatitude = -98.524;
+        final double defaultLongtitude = 20.304;
+
+        private static Double latitude, longtitude;
+        protected Location mLastLocation;
         //LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
 
         private DatabaseReference mDatabase, rDatabase;
@@ -365,6 +379,8 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
         @Override
         public void onCreate(Bundle savedInstanceState) {
             try {
+
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(super.getActivity());
                 super.onCreate(savedInstanceState);
                 database = FirebaseDatabase.getInstance();
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -380,6 +396,13 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = null;
             try {
+
+                if(!checkPermission()){
+                    requestPermissions();
+
+                }else{
+                    getLastLocation();
+                }
                 rootView = inflater.inflate(R.layout.mainscreen_tab, container, false);
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
 
@@ -415,12 +438,147 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
             return rootView;
         }
 
+        @SuppressWarnings("MissingPermission")
+        private void getLastLocation() {
+            mFusedLocationClient.getLastLocation()
+                    .addOnCompleteListener(super.getActivity(), new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                mLastLocation = task.getResult();
+
+                                latitude= mLastLocation.getLatitude();
+                                longtitude= mLastLocation.getLongitude();
+                            } else {
+                                Log.w(TAG, "getLastLocation:exception", task.getException());
+                                showSnackbar(getString(R.string.no_location_detected));
+                            }
+                        }
+                    });
+        }
+
+        /**
+         * Shows a {@link Snackbar} using {@code text}.
+         *
+         * @param text The Snackbar text.
+         */
+        private void showSnackbar(final String text) {
+            View container = super.getView().findViewById(R.id.appbar);
+            if (container != null) {
+                Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+            }
+        }
+
+        /**
+         * Shows a {@link Snackbar}.
+         *
+         * @param mainTextStringId The id for the string resource for the Snackbar text.
+         * @param actionStringId   The text of the action item.
+         * @param listener         The listener associated with the Snackbar action.
+         */
+        private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                                  View.OnClickListener listener) {
+            Snackbar.make(super.getView().findViewById(android.R.id.content),
+                    getString(mainTextStringId),
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(actionStringId), listener).show();
+        }
+
+        /**
+         * Return the current state of the permissions needed.
+         */
+        private boolean checkPermission() {
+            int permissionState = ActivityCompat.checkSelfPermission(super.getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION);
+            return permissionState == PackageManager.PERMISSION_GRANTED;
+        }
+
+        private void startLocationPermissionRequest() {
+            ActivityCompat.requestPermissions(super.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_REQUEST_LOCATION);
+        }
+
+        private void requestPermissions() {
+            boolean shouldProvideRationale =
+                    ActivityCompat.shouldShowRequestPermissionRationale(super.getActivity(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION);
+
+            // Provide an additional rationale to the user. This would happen if the user denied the
+            // request previously, but didn't check the "Don't ask again" checkbox.
+            if (shouldProvideRationale) {
+                Log.i(TAG, "Displaying permission rationale to provide additional context.");
+
+                showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Request permission
+                                startLocationPermissionRequest();
+                            }
+                        });
+
+            } else {
+                Log.i(TAG, "Requesting permission");
+                // Request permission. It's possible this can be auto answered if device policy
+                // sets the permission in a given state or the user denied the permission
+                // previously and checked "Never ask again".
+                startLocationPermissionRequest();
+            }
+        }
+
+        /**
+         * Callback received when a permissions request has been completed.
+         */
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                               @NonNull int[] grantResults) {
+            Log.i(TAG, "onRequestPermissionResult");
+            if (requestCode == PERMISSION_REQUEST_LOCATION) {
+                if (grantResults.length <= 0) {
+                    // If user interaction was interrupted, the permission request is cancelled and you
+                    // receive empty arrays.
+                    Log.i(TAG, "User interaction was cancelled.");
+                } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted.
+                    getLastLocation();
+                } else {
+                    // Permission denied.
+
+                    // Notify the user via a SnackBar that they have rejected a core permission for the
+                    // app, which makes the Activity useless. In a real app, core permissions would
+                    // typically be best requested during a welcome-screen flow.
+
+                    // Additionally, it is important to remember that a permission might have been
+                    // rejected without asking the user for permission (device policy or "Never ask
+                    // again" prompts). Therefore, a user interface affordance is typically implemented
+                    // when permissions are denied. Otherwise, your app could appear unresponsive to
+                    // touches or interactions which have required permissions.
+                    showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                            new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    // Build intent that displays the App settings screen.
+                                    Intent intent = new Intent();
+                                    intent.setAction(
+                                            Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                    Uri uri = Uri.fromParts("package",
+                                            BuildConfig.APPLICATION_ID, null);
+                                    intent.setData(uri);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                }
+                            });
+                }
+            }
+        }
         @Override
         public void onMapReady(GoogleMap googleMap) {
             try {
+
                 final GoogleMap mMap = googleMap;
 
-                LatLng userMarker = new LatLng(29.304, -98.524);
+                LatLng userMarker = new LatLng(latitude, longtitude);
                 mMap.addMarker(new MarkerOptions().position(userMarker).title("Me"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userMarker, 18));
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
@@ -485,6 +643,37 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
                 e.printStackTrace();
             }
         }
+   
+        private void requestLocationPermission() {
+               // Permission has not been granted and must be requested.
+               if (ActivityCompat.shouldShowRequestPermissionRationale(super.getActivity(),
+                       Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                   // Provide an additional rationale to the user if the permission was not granted
+                   // and the user would benefit from additional context for the use of the permission.
+                   // Display a SnackBar with a button to request the missing permission.
+                   Snackbar.make(super.getView(), "Location Access is required to display markers  ",
+                           Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                       @Override
+                       public void onClick(View view) {
+                           // Request the permission
+                           MainScreenTabFragment.super.getActivity().requestPermissions(
+                                   new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                   PERMISSION_REQUEST_LOCATION);
+                       }
+                   }).show();
+
+               } else {
+
+                   Snackbar.make(super.getView(),
+                           "Permission is not available. Requesting Location permission.",
+                           Snackbar.LENGTH_SHORT).show();
+                   // Request the permission. The result will be received in onRequestPermissionResult().
+                   super.getActivity().requestPermissions( new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                           PERMISSION_REQUEST_LOCATION);
+               }
+        }
+
+
 
         @Override
         public void onResume() {
@@ -492,6 +681,7 @@ public class Main2Activity extends AppCompatActivity implements MyDialogFragment
                 super.onResume();
                 mapView.onResume();
                 mGoogleApiClient.connect();
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
