9a0091382c1c410b3eddd24d383bc1d8baea67dc
Chris gutierrez <chris201234@gmail.com>
2018-03-25 17:43:44 -0500

    modified:   MainActivity.java
    Login Persistence

--- app/src/main/java/com/rendevu/main/MainActivity.java
+++ app/src/main/java/com/rendevu/main/MainActivity.java
@@ -8,21 +8,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends Activity {
-
+    private FirebaseAuth auth;
     Intent i=null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        auth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
+                if (auth.getCurrentUser() != null) {
+                    Toast.makeText(this, "User: "+auth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+                  startActivity(new Intent(this, Main2Activity.class));
+                  finish();
+              }
     }
 
     public void login_register(View v)
