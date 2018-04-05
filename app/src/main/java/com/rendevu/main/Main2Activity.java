da5f034fe4ac9f7f041a4682f440ca765d40fbb0
Chris gutierrez <chris201234@gmail.com>
2018-04-04 19:13:54 -0500

    modified:   Main2Activity.java  	new file:   SectionsPagerAdapter.java
    pulled the SectionsPagerAdapter class from main2 activity
    and removed some unnecessary imports

--- app/src/main/java/com/rendevu/main/Main2Activity.java
+++ app/src/main/java/com/rendevu/main/Main2Activity.java
@@ -5,109 +5,36 @@ package com.rendevu.main;
  *  This implements each of the three fragment.
  */
 
-import android.*;
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
-import android.os.Looper;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.Adapter;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
-//import com.google.android.gms.awareness.snapshot.LocationResult;
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
 import com.google.android.gms.appinvite.AppInviteInvitation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Queue;
 import java.lang.NullPointerException;
 
-import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
-
-//import javax.xml.crypto.Data;
 
 
 public class Main2Activity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
-    private static final int PERMISSION_REQUEST_LOCATION = 34;
-
 
     String s = "Null pointer exception for user.getUid()";
-
-
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -252,10 +179,6 @@ public class Main2Activity extends AppCompatActivity implements ActivityCompat.O
             e.printStackTrace();
         }
     }
-            
-       
-
-
 
                               
     /**
@@ -329,8 +252,6 @@ public class Main2Activity extends AppCompatActivity implements ActivityCompat.O
      *END LOGOUT METHOD**
      ********************/
 
-
-
     /*
     * Josh
     *   Back button when already logged in will minimize the app.
@@ -341,14 +262,6 @@ public class Main2Activity extends AppCompatActivity implements ActivityCompat.O
         this.moveTaskToBack(true);
     }
 
-
-
-
-
-
-
-
-                              
     /**
      * Josh
      * Controls for the pop-up dialog fragment,
@@ -381,50 +294,4 @@ public class Main2Activity extends AppCompatActivity implements ActivityCompat.O
     }
 
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            try {
-                switch (position) {
-                    case 0:
-                        return new contactActivity();
-                    case 1:
-                        return new mapActivity();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            try {
-                switch (position) {
-                    case 0:
-                        return "Contacts";
-                    case 1:
-                        return "Main";
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }
 }
